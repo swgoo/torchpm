27e1578d01dba474544ba92725adb4b0435411f0
yeoun9 <yeoun9@gmail.com>
2021-04-09 16:58:39 +0900

    Update __init__.py

--- torchpm/__init__.py
+++ torchpm/__init__.py
@@ -1,4 +1,4 @@
 from .misc import *
-from .data import CSVDataset
+from . import data, diff, funcgen, linearode, loss, misc, model, predfunction, scale
 __version__ = "0.0.1"
 # __all__ = ['data', 'diff', 'funcgen', 'linearode', 'loss', 'misc', 'model', 'predfunction', 'scale']
