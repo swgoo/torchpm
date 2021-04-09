7ac979a6c59ff77a1d40bca2094cc31c64a8e9dd
yeoun9 <yeoun9@gmail.com>
2021-04-09 17:12:21 +0900

    Update __init__.py

--- torchpm/__init__.py
+++ torchpm/__init__.py
@@ -1,4 +1,3 @@
-from .misc import *
 from . import data, diff, funcgen, linearode, loss, misc, model, predfunction, scale
 __version__ = "0.0.1"
 # __all__ = ['data', 'diff', 'funcgen', 'linearode', 'loss', 'misc', 'model', 'predfunction', 'scale']
