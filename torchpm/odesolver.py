e8ca58058023743e12ec7ed441feb4d21b46e205
yeoun9 <yeoun9@gmail.com>
2022-05-17 09:39:14 +0900

    .

--- torchpm/odesolver.py
+++ torchpm/odesolver.py
@@ -1,27 +1,46 @@
 from copy import deepcopy
-from typing import ClassVar, List, Optional, Dict, Iterable, Union
+from functools import cache
+import pickle
+from re import X
+from typing import ClassVar, List, Optional, Dict, Iterable, Tuple, Union
 import torch as tc
 import numpy as np
 import sympy as sym
 import sympytorch as spt
 from torch import nn
+import torch
+import shelve
+
+class HashableDict(dict):
+    def __hash__(self):
+        return hash(tuple(sorted(self.items())))
 
 class CompartmentModelGenerator(nn.Module) :
-    def __init__(self, distribution_bool_matrix: List[List[bool]], has_depot : bool = False, transit : int = 0, observed_compartment_num = 0, administrated_compartment_num = 0, is_infusion : bool= False) -> None:
+    def __init__(self, distribution_bool_matrix: Union[Tuple[Tuple[bool]], bool], has_depot : bool = False, transit : int = 0, observed_compartment_num = 0, administrated_compartment_num = 0, is_infusion : bool= False) -> None:
         super().__init__()
 
-        self.distribution_bool_matrix = deepcopy(distribution_bool_matrix)
+        
+
+        self.distribution_bool_matrix = []
+        if isinstance(distribution_bool_matrix, bool) :
+            self.distribution_bool_matrix = [[distribution_bool_matrix]]
+        elif isinstance(distribution_bool_matrix, Tuple) :
+            for row in list(distribution_bool_matrix) :
+                self.distribution_bool_matrix.append(list(row))
+
         self.is_infusion = is_infusion
-        self.obeserved_compartment_num = observed_compartment_num
+        self.observed_compartment_num = observed_compartment_num
         self.administrated_compartment_num = administrated_compartment_num
         self.depot_compartment_num = administrated_compartment_num
+        self.has_depot = has_depot
+        self.transit = transit
         if has_depot :
             self.distribution_bool_matrix.append([False] * len(self.distribution_bool_matrix))
             for row in self.distribution_bool_matrix :
                 row.append(False)
             self.distribution_bool_matrix[-1][self.administrated_compartment_num] = True
         
-            self.depot_compartment_num = len(distribution_bool_matrix)
+            self.depot_compartment_num = len(self.distribution_bool_matrix) - 1
 
         if has_depot and transit > 0 :
             self.distribution_bool_matrix[-1][self.administrated_compartment_num] = False
@@ -46,12 +65,13 @@ class CompartmentModelGenerator(nn.Module) :
 
         if is_infusion :
         
-            t_sym, dose_sym, r_sym = sym.symbols('t d r', positive = True, real=True, allow_half=False)
+            t_sym = sym.symbols('t', real=True, negative = False, finite = True)
+            r_sym, dose_sym = sym.symbols('r, d', real=True, positive = True, finite = True)
             dCdts_infusion = self._get_dCdts(is_infusion=True)
             initial_states_infusion = self._get_initial_states(is_infusion=True)
             cs_infusion = self._solve(dCdts_infusion, initial_states_infusion)
 
-            initial_states[sym.symbols('c_' + str(self.depot_compartment_num), cls=sym.Function)(0)] = cs_infusion[self.depot_compartment_num].rhs.subs({t_sym: dose_sym/r_sym})  # type: ignore
+            initial_states[sym.symbols('c_' + str(self.depot_compartment_num), cls=sym.Function, real = True, negative=False, finite = True)(0)] = cs_infusion[self.depot_compartment_num].rhs.subs({t_sym: dose_sym/r_sym})  # type: ignore
             cs = self._solve(dCdts, initial_states)
             
             for i in range(len(cs)) :
@@ -61,21 +81,17 @@ class CompartmentModelGenerator(nn.Module) :
             comps_infusion = []
             for comp in cs_infusion :
                 comps_infusion.append(comp.rhs)
-            self.infusion_model = spt.SymPyModule(expressions=comps_infusion, extra_funcs={sym.core.numbers.Half: lambda : tc.tensor(1/2)})
-
-            
-                # sym.core.numbers.Rational: lambda p, q=None: p/q
-
+            self.infusion_model = spt.SymPyModule(expressions=comps_infusion)
+        
         comps = []
         for comp in cs :
             comps.append(comp.rhs)
-        #TODO 제거
-        # comps.append(sym.Eq(sym.core.numbers.Half, tc.tensor(1/2), sympify=False))
-        self.model = spt.SymPyModule(expressions=comps, extra_funcs={sym.core.numbers.Half: lambda : tc.tensor(1/2)})
+        self.model = spt.SymPyModule(expressions=comps)
     
     
 
-    def _check_square_matrix(self, m : List[List[bool]], error_massage) :
+    def _check_square_matrix(self, m : list[list[bool]], error_massage) :
+        
         length = len(m)
         for row in m :
             if len(row) != length :
@@ -87,8 +103,8 @@ class CompartmentModelGenerator(nn.Module) :
             ics: initial states of compartments
         """
         comps_num = len(self.distribution_bool_matrix)
-        comps = [sym.symbols("c_"+ str(i), cls=sym.Function) for i in range(comps_num)]  # type: ignore
-        d = sym.symbols('d', allow_half=False) #dose
+        comps = [sym.symbols("c_"+ str(i), cls=sym.Function, real = True, negative = False, finite = True) for i in range(comps_num)]  # type: ignore
+        d = sym.symbols('d', real = True, Positve = True, finite = True) #dose
 
         ics = {comp(0): 0 for comp in comps}
         ics[comps[self.depot_compartment_num](0)] = 0 if is_infusion else d
@@ -103,21 +119,21 @@ class CompartmentModelGenerator(nn.Module) :
 
         comps_num = len(self.distribution_bool_matrix)
 
-        t = sym.symbols('t', positive = True, real=True, allow_half=False) #time
-        r = sym.symbols('r', positive = True, real=True, allow_half=False) #infusion rate
+        t = sym.symbols('t', negative = False, real=True, finite = True) #time
+        r = sym.symbols('r', positive = True, real=True, finite = True) #infusion rate
         
-        comps = [sym.symbols("c_"+ str(i), cls=sym.Function) for i in range(comps_num)]  # type: ignore
+        comps = [sym.symbols("c_"+ str(i), cls=sym.Function, negative = False, real = True, finite = True) for i in range(comps_num)]  # type: ignore
 
         elimination_rate = sym.eye(comps_num)
         for i in range(comps_num) :
-            elimination_rate[i,i] = sym.symbols('k_' + str(i) + str(i), positive = True, real=True, allow_half=False) if self.distribution_bool_matrix[i][i] else 0
+            elimination_rate[i,i] = sym.symbols('k_' + str(i) + str(i), positive = True, real=True, finite = True) if self.distribution_bool_matrix[i][i] else 0
 
         distribution_rate = sym.zeros(comps_num, comps_num)
         for i in range(comps_num) :
             for j in range(comps_num) :
                 if i == j or not self.distribution_bool_matrix[i][j]:
                     continue
-                distribution_rate[i,j] = sym.symbols('k_'+str(i)+str(j), positive = True, real=True, allow_half=False)
+                distribution_rate[i,j] = sym.symbols('k_'+str(i)+str(j), positive = True, real=True, finite = True)
         
         comps_matrix = sym.Matrix([comps[i](t) for i in range(comps_num)])
         dcdt_eqs = distribution_rate.T * comps_matrix \
@@ -137,9 +153,49 @@ class CompartmentModelGenerator(nn.Module) :
             ics: initial states of compartments
         Return :
             compartment functions by time
+
+            
         """
-        function = sym.solvers.ode.systems.dsolve_system(dCdts, ics=ics, doit=True)
-        return function[0]
+        comps_num = len(dCdts)
+        db = shelve.open('./ode.db')
+        
+        parameters = dict(
+                distribution_bool_matrix=self.distribution_bool_matrix, 
+                has_depot=self.has_depot, 
+                transit=self.transit, 
+                observed_compartment_num=self.observed_compartment_num, 
+                administrated_compartment_num=self.administrated_compartment_num, 
+                is_infusion=self.is_infusion)
+        parameters = HashableDict(parameters)
+
+        comps = [sym.symbols("c_"+ str(i), cls=sym.Function, negative = False, real = True)(sym.symbols('t', negative = False, real = True)) for i in range(comps_num)]  # type: ignore
+
+        try :
+            function_serialized = db[str(parameters)]
+            function = []
+            for i, f in enumerate(function_serialized) :
+                rhs = pickle.loads(f)
+                lhs = comps[i]
+                function.append(sym.Eq(lhs, rhs, simpify=False))
+            return function
+        except KeyError :
+            function = sym.solvers.ode.systems.dsolve_system(dCdts, ics=ics, doit=True, simplify=True)[0]
+            function_serialized = []
+            for f in function :
+                function_serialized.append(pickle.dumps(f.rhs))
+            db[str(parameters)] = function_serialized
+            return function
+        finally :
+
+        # function = sym.solvers.ode.systems.dsolve_system(dCdts, ics=ics, doit=True, simplify=True)[0]
+        # function_serialized = []
+        # for f in function :
+        #     function_serialized.append(pickle.dumps(f.rhs))
+        # db[str(parameters)] = function_serialized
+            db.close()
+        # return function
+
+        
 
     def forward(self, t, **variables):
         
